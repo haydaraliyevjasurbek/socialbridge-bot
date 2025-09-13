c73641c5ceed1cb6e19348b6fdf6e178a4739963
Jasurbek Haydaraliyev <129217406+haydaraliyevjasurbek@users.noreply.github.com>
2025-09-13 09:17:48 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI); // opsiyalarni olib tashladik
     console.log("MongoDB ulanishi ✅");
   } catch (err) {
     console.error("MongoDB xato:", err);
