09012c9d04ace9ed2b912b3115e2a56d086b0289
Jasurbek Haydaraliyev <129217406+haydaraliyevjasurbek@users.noreply.github.com>
2025-09-13 11:54:07 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,21 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 export const saveUser = async (msg) => {
   try {
-    const chatId = msg.chat.id;
-    const firstName = msg.from.first_name;
-    const lastName = msg.from.last_name || "";
-    const username = msg.from.username || "";
-
-    const existing = await User.findOne({ chatId });
-    if (!existing) {
-      const newUser = new User({ chatId, firstName, lastName, username });
+    const { id, first_name, last_name, username } = msg.from;
+    const existingUser = await User.findOne({ chatId: id });
+    if (!existingUser) {
+      const newUser = new User({
+        chatId: id,
+        firstName: first_name,
+        lastName: last_name,
+        username: username,
+        date: new Date()
+      });
       await newUser.save();
+      console.log(`Yangi foydalanuvchi saqlandi: ${first_name}`);
     }
   } catch (err) {
     console.error("saveUser xato:", err);
   }
-};
-
-export const getAllUsers = async () => {
-  return await User.find({});
 };
